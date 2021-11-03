fd2d6449fa47277dafab206d6036bcd115a3b4e7
Mithun Reddy <mithunreddy87@gmail.com>
2021-11-03 15:45:21 +0530

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import GoogleFonts from "next-google-fonts";
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
 import theme from "../styles/theme";
 
 export default class MyDocument extends NextDocument {
@@ -9,9 +10,19 @@ export default class MyDocument extends NextDocument {
       <Html lang="en">
         <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" />
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
           <script
             dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];`,
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });`,
             }}
           />
           <link rel="icon" href="/edit-2.svg" />
